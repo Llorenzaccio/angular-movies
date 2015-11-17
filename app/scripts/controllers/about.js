2fa33df59613bcf5cfa7da56cfe0344bb252c935
ghjuvan <vaevconsult@gmail.com>
2015-11-17 12:07:08 +0100

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name certificationAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the certificationAngularApp
+ */
+angular.module('certificationAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name certificationAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the certificationAngularApp
+ */
+angular.module('certificationAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
